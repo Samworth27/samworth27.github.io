bc39438a57a40ed0eec48d2ab86bfcf4c25bab8d
Sam Mitchell <samworth27@gmail.com>
2022-06-07 08:55:46 +1000

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,30 +4,30 @@ module.exports = {
     extend: {
       backgroundImage: {
         "section-1-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516713/portfolio/truck.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516713/portfolio/truck.jpg')",
         "section-2-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516705/portfolio/boats.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516705/portfolio/boats.jpg')",
         "section-3-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516704/portfolio/bamaga.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516704/portfolio/bamaga.jpg')",
         "section-4-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516702/portfolio/cape.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_920,w_1920/v1654516702/portfolio/cape.jpg')",
         "section-5-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1080,w_1920/v1654516705/portfolio/bamaga2.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1080,w_1920/v1654516705/portfolio/bamaga2.jpg')",
         "section-6-landscape":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1080,w_1920/v1654516700/portfolio/sundowner.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1080,w_1920/v1654516700/portfolio/sundowner.jpg')",
         "section-1-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516713/portfolio/truck.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516713/portfolio/truck.jpg')",
         "section-2-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516705/portfolio/boats.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516705/portfolio/boats.jpg')",
         "section-3-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516704/portfolio/bamaga.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516704/portfolio/bamaga.jpg')",
         "section-4-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516702/portfolio/cape.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516702/portfolio/cape.jpg')",
         "section-5-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516705/portfolio/bamaga2.jpg')",
+          "linear-gradient(180deg,rgba(0, 0, 0, 0.6) 0%,rgba(0, 0, 0, 0.3) 100%), url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516705/portfolio/bamaga2.jpg')",
         "section-6-portrait":
-          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_auto,h_1000,w_530/v1654516700/portfolio/sundowner.jpg')",
-        "topo": "url('./src/img/topography.svg'),linear-gradient(#e6646485, #9198e585)"
+          "url('https://res.cloudinary.com/xxvii/image/upload/c_fill,g_xy_center,h_1000,w_530,x_2000,y_0/v1654516700/portfolio/sundowner.jpg')",
+        topo: "url('./src/img/topography.svg'),",
       },
     },
   },
